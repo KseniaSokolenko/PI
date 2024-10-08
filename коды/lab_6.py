9feca54b2e431d489b12f21d04d66669525a1ab5
Ksenia Sokolenko <163596266+KseniaSokolenko@users.noreply.github.com>
2024-10-08 18:06:10 +0500

    Add files via upload

---
+++ коды/lab_6.py
@@ -0,0 +1,9 @@
+Winx = ['Bloom', 'Stella', 'Flora', 'Musa', 'Aisha', 'Tecna']
+Eva, Sasha, Nika, Nastya, Vika, Ksusha = Winx
+print('Эва - ', Eva)
+print('Саша - ', Sasha)
+print('Ника - ', Nika)
+print('Настя - ', Nastya)
+print('Вика - ', Vika)
+print('Ксюша - ', Ksusha)
+
