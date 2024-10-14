f64cd21fb4af56c99078ccb79a54de7d357b122c
Ksenia Sokolenko <163596266+KseniaSokolenko@users.noreply.github.com>
2024-10-14 21:51:47 +0500

    Delete коды/s2.py

--- коды/s2.py
+++
@@ -1,9 +0,0 @@
-def remover(tup, value):
-    if value in tup:
-        return tuple(i for i in tup if i != value)
-    else:
-        return tup
-
-print(remover((1, 2, 3), 1))
-print(remover((1, 2, 3, 1, 2, 3, 4, 5, 2, 3, 4, 2, 4, 2), 3))
-print(remover((2, 4, 6, 6, 4, 2), 9))
